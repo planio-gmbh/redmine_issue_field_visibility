07406f2aa80a89d20c813996693a5928fe7ee530
Felix Schäfer <felix@plan.io>
2016-06-17 18:44:54 +0200

    Fix hiding in emails

--- lib/redmine_issue_field_visibility/issue_patch.rb
+++ lib/redmine_issue_field_visibility/issue_patch.rb
@@ -35,9 +35,14 @@ module RedmineIssueFieldVisibility
         end
       end
 
+      # WARNING: if changed here change in journal_patch too
       def each_notification(users, &block)
         users.group_by do |user|
-          user.roles_for_project(project).sort
+          if user.admin?
+            :admin
+          else
+            user.roles_for_project(project).sort
+          end
         end.values.each do |part|
           super(part, &block)
         end
