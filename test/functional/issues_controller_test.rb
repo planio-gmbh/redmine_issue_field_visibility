c4081275f811e693d71950dd656450c4492b3fcc
Felix Schäfer <felix@plan.io>
2022-06-27 09:42:22 +0200

    Use Redmine::ControllerTest

--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-class RedmineIssueFieldVisibilityIssuesControllerTest < ActionController::TestCase
+class RedmineIssueFieldVisibilityIssuesControllerTest < Redmine::ControllerTest
   fixtures :projects,
            :users,
            :roles,
